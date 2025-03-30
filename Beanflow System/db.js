3f34feb08561de67fd366c76f54935aa75455353
lucasazevedd <lucasazevedd@hotmail.com>
2025-03-30 13:05:22 -0300

    add backend and database

---
+++ Beanflow System/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect()
+  .then(() => console.log("✅ Conectado ao PostgreSQL!"))
+  .catch(err => console.error("❌ Erro ao conectar ao PostgreSQL:", err));
+
+module.exports = pool;

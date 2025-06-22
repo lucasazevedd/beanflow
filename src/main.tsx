48ceb6532b0fb379cad4bb1dee3f5a0c95b6980a
lucasazevedd <lucasazevedd@hotmail.com>
2025-06-22 13:26:59 -0300

    melhorias pro mobile

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,17 @@ import './index.css';
 // 🔁 Importa as rotas da aplicação
 import AppRoutes from './routes';
 
+import { registerSW } from 'virtual:pwa-register';
+
+if ('serviceWorker' in navigator) {
+  const updateSW = registerSW({
+    immediate: true,
+    onNeedRefresh() {
+      updateSW(true); // atualiza ao detectar novo SW
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppRoutes />
